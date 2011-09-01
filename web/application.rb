4f4fe1d648f239a6e53bb847ae19f344f20a1b8c
Ash McKenzie <ash.mckenzie@playup.com>
2011-09-01 13:21:56 +1000

    Enable caching.

--- web/application.rb
+++ web/application.rb
@@ -14,7 +14,7 @@ end
 
 get '/search/query/:query' do
   content_type :json
-  #cache_control :public, :max_age => 36000
+  cache_control :public, :max_age => 36000
   results = $toilets.search(params[:query], 100).to_hash[:allocations].collect { |x| x[4] }.flatten
   Toilet.json_rep Toilet.all(:id => results)
 end
